a27220f618c1e472c596f3b726c51cb9d55e8b0c
psalmistcom <dadewale9@gmail.com>
2023-06-26 22:01:02 +0100

    property model

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Broker extends Model
+class Property extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'name', 'address', 'city', 'zip_code', 'phone_number', 'logo_path'
-    ];
 }
---
+++ database/migrations/2023_06_26_205534_create_properties_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('broker_id');
+            $table->string('address');
+            $table->enum('listing_type', []);
+            $table->string('city');
+            $table->string('zip_code');
+            $table->longText('description');
+            $table->year('build_year');
+            $table->timestamps();
+
+            $table->foreign('broker_id')
+                ->references('id')
+                ->on('brokers')
+                ->cascadeOnDelete();
+
+            $table->unique(['address', 'zip_code']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+};
