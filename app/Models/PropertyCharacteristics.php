7fca0e0acc4f9a80922a8fd8f71cff342c685a12
psalmistcom <dadewale9@gmail.com>
2023-06-28 19:44:35 +0100

    property and property_characteristics models

--- app/Models/PropertyCharacteristics.php
+++ app/Models/PropertyCharacteristics.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class PropertyCharacteristics extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'property_id', 'price', 'bedrooms', 'bathrooms', 'sqft', 'price_sqft', 'property_type', 'status'
+    ];
 }
